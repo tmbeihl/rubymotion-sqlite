378bb9b26416c86361330cfdd80368f26a21fd69
Senthil Nayagam <senthil@railsfactory.org>
2012-05-24 21:16:55 +0530

    updated ignored file

---
+++ lib/migration.rb
@@ -0,0 +1,34 @@
+class Migration
+  
+
+
+end
+
+
+=begin
+
+
+database.create_table(tablename, columns)
+database.drop_table(tablename)
+database.rename_table(oldtablename, newtablename)
+database.action.to_sql
+
+
+
+
+table.add_column(columnname,type, constraint)
+table.remove_column(columnname)
+
+table.rename_column(oldname,newname)
+table.add_constraint(column,constraint)
+
+
+table.action.to_sql
+
+
+
+
+
+
+
+=end  
